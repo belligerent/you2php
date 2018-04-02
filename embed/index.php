67c1d09169b31be68a2f3b363d98c2744328eb10
Belligerent <neothera@live.ru>
2018-04-02 12:55:15 +0800

    Update index.php
    change js cdm

--- embed/index.php
+++ embed/index.php
@@ -8,7 +8,7 @@ if(!is_array($_GET)&&count($_GET)>0){ header("Location: ../error.php"); exit();}
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <base href="<?php echo trim(Root_part()," embed/ ") ?>/" />
         <link href="//cdn.bootcss.com/video.js/5.20.4/alt/video-js-cdn.min.css" rel="stylesheet" />
-        <script src="//libs.cdnjs.net/video.js/5.20.4/video.min.js"></script>
+        <script src="//cdn.bootcss.com/video.js/5.20.4/video.min.js"></script>
         <script type="text/javascript" src="./inc/4.js"></script>
         <link rel="stylesheet" href="./inc/theme.css" type="text/css">
     </head>
